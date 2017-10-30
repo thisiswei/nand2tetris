a5abf88f75f7356a51e5d414d4ccb3905f69bc56
thisiswei <99plus2@gmail.com>
2017-10-30 10:45:09 -0400

    [project 8] goto

--- nand2tetris-2/projects/07/parse.py
+++ nand2tetris-2/projects/07/parse.py
@@ -39,6 +39,9 @@ def parser(string):
     if parts['command'] == 'label':
         return label_helper(parts)
 
+    if parts['command'] == 'goto':
+        return goto_helper(parts)
+
     if parts['command'] == 'push':
         return push_helper(parts)
 
@@ -62,6 +65,13 @@ def parser(string):
     }.get(command)
     if command_runner:
         return command_runner()
+
+
+def goto_helper(parts):
+    return [
+        '@{}'.format(parts['segment']),
+        '0;JMP',
+    ]
 
 
 def label_helper(parts):
@@ -711,6 +721,39 @@ xs = [
     'push argument 0',
     'if-goto LOOP_START',
     'push local 0',
+]
+
+
+xs = [
+    'push argument 1',
+    'pop pointer 1',
+    'push constant 0',
+    'pop that 0',
+    'push constant 1',
+    'pop that 1',
+    'push argument 0',
+    'push constant 2',
+    'sub',
+    'pop argument 0',
+    'label MAIN_LOOP_START',
+    'push argument 0',
+    'if-goto COMPUTE_ELEMENT',
+    'goto END_PROGRAM',
+    'label COMPUTE_ELEMENT',
+    'push that 0',
+    'push that 1',
+    'add',
+    'pop that 2',
+    'push pointer 1',
+    'push constant 1',
+    'add',
+    'pop pointer 1',
+    'push argument 0',
+    'push constant 1',
+    'sub',
+    'pop argument 0',
+    'goto MAIN_LOOP_START',
+    'label END_PROGRAM',
 ]
 
 
